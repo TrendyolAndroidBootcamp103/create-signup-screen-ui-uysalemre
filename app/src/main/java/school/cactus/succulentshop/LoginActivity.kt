370c29b0372e02282949e387abd3cc92536a6e2e
emreuysal <uysalemre02@gmail.com>
2021-04-28 22:32:37 +0300

    IssueNo : 1 Commit Message : Add SignUp Screen and Validations

--- app/src/main/java/school/cactus/succulentshop/LoginActivity.kt
+++ app/src/main/java/school/cactus/succulentshop/LoginActivity.kt
@@ -1,42 +1,48 @@
 package school.cactus.succulentshop
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.textfield.TextInputLayout
 import school.cactus.succulentshop.databinding.ActivityLoginBinding
+import school.cactus.succulentshop.helpers.validate
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityLoginBinding
-
-    private val identifierValidator = IdentifierValidator()
-    private val passwordValidator = PasswordValidator()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setPageConfigurations()
+    }
 
-        supportActionBar?.title = getString(R.string.log_in)
-
-        binding.apply {
-            logInButton.setOnClickListener {
-                passwordInputLayout.validate()
-                identifierInputLayout.validate()
-            }
+    override fun onClick(view: View?) {
+        when (view) {
+            binding.createAccountButton -> navigateToSignUp()
+            binding.logInButton -> navigateToMain()
         }
     }
 
-    private fun TextInputLayout.validate() {
-        val errorMessage = validator().validate(editText!!.text.toString())
-        error = errorMessage?.resolveAsString()
-        isErrorEnabled = errorMessage != null
+    private fun setPageConfigurations() {
+        supportActionBar?.title = getString(R.string.log_in)
+        binding.createAccountButton.setOnClickListener(this)
+        binding.logInButton.setOnClickListener(this)
     }
 
-    private fun Int.resolveAsString() = getString(this)
+    private fun navigateToSignUp() {
+        startActivity(Intent(this, SignupActivity::class.java))
+    }
 
-    private fun TextInputLayout.validator() = when (this) {
-        binding.identifierInputLayout -> identifierValidator
-        binding.passwordInputLayout -> passwordValidator
-        else -> throw IllegalArgumentException("Cannot find any validator for the given TextInputLayout")
+    private fun navigateToMain() {
+        with(binding) {
+            val isIdentifierValid = identifierInputLayout.validate(this)
+            val isPasswordValid = passwordInputLayout.validate(this)
+            if (isIdentifierValid && isPasswordValid) {
+                // TODO handle service response and navigate to main activity of succulent shop
+                Toast.makeText(this@LoginActivity, "Login Success", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
